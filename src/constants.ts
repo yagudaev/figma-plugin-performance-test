89c2a72e638f4de763ec20b0c15ce3d955b8f4b2
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-12-08 15:38:33 -0800

    Add chunking

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const COUNT_TARGET = 5_600_000_000
+export const COUNT_TARGET = 500_000_000
--- src/main/actions/index.ts
+++ src/main/actions/index.ts
@@ -1,3 +1,4 @@
+import Timeout from "await-timeout"
 import { COUNT_TARGET } from "../../constants"
 
 export async function count() {
@@ -7,14 +8,33 @@ export async function count() {
   for (let i = 0; i < COUNT_TARGET; i++) {
     value = i
   }
+  console.log("[main] Finished counting...")
   return value
 }
 
+// TOOD: how do you determine the optimal chunk size?
+const CHUNK_SIZE = 500_000 // 5 million in 10ms counted
 export async function countChunked() {
-  console.log("[main] Starting to count...")
+  console.log("[main] Starting to count chunked...")
 
+  const numChunks = Math.ceil(COUNT_TARGET / CHUNK_SIZE)
   let value = 0
-  for (let i = 0; i < COUNT_TARGET; i++) {
+  for (let i = 0; i < numChunks; i++) {
+    const from = i * CHUNK_SIZE
+    const to = Math.min((i + 1) * CHUNK_SIZE, COUNT_TARGET)
+    value = countChunk(from, to)
+
+    // wait for next tick to avoid blocking the main thread
+    await Timeout.set(0)
+  }
+
+  console.log("[main] Finished counting chunked...")
+  return value
+}
+
+function countChunk(from: number, to: number) {
+  let value = 0
+  for (let i = from; i < to; i++) {
     value = i
   }
   return value
