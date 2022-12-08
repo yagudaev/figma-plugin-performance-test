cb8fdda5e2d1d5f58cfd379d287c45b4a0c71230
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-12-07 20:58:56 -0800

    Generalize a bit more

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import Timeout from "await-timeout"
 import { ReqCountHandler, ResCountHandler } from "./api"
 
 export default function () {
-  handleCount(async function () {
-    console.log("It's the final countdown! Tananana nanana nanana")
-    await Timeout.set(1000)
-    return 1000
-  })
+  exposeToUI(count)
   showUI({ height: 240, width: 320 })
 }
 
-function handleCount(fn: () => Promise<number>) {
+async function count() {
+  console.log("It's the final countdown! Tananana nanana nanana")
+  await Timeout.set(1000)
+  return 1000
+}
+
+function exposeToUI(fn: () => Promise<number>) {
+  console.log("called by function with name", fn.name)
   on<ReqCountHandler>("REQ_COUNT", async () => {
     const returnValue = await fn()
     emit<ResCountHandler>("RES_COUNT", returnValue)
