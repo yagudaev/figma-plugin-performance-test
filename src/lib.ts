236fc92ba5e4efc89fe7d17102738b35e9e33fe7
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-12-09 15:25:10 -0800

    Do subscriptions cleanup

--- src/lib.ts
+++ src/lib.ts
@@ -6,21 +6,27 @@ export function callMain(fnName: string, ...args: any[]) {
   lastCallerId += 1
   const callerId = lastCallerId
 
-  args = args.map((arg) => checkForCallbacks(fnName, arg))
+  args = args.map((arg) => checkForCallbacks(fnName, callerId, arg))
 
   return new Promise<any>(function (resolve) {
-    // TODO: unsubscribe from all subscriptions once we get a response
-    once(`RES_${fnName}_${callerId}`, (returnValue) => resolve(returnValue))
+    once(`RES_${fnName}_${callerId}`, (returnValue) => {
+      resolve(returnValue)
+      const unsubscribes = subscriptions.get(getSubscriptionStringPrefix(fnName, callerId))
+      unsubscribes?.forEach((unsubscribe) => unsubscribe())
+      subscriptions.set(getSubscriptionStringPrefix(fnName, callerId), [])
+      console.log("[ui] cleaned up subscriptions", unsubscribes?.length)
+    })
     emit(`REQ_${fnName}`, callerId, ...args)
   })
 }
 
-function checkForCallbacks(fnName: string, arg: any) {
+const subscriptions = new Map<string, Function[]>()
+function checkForCallbacks(fnName: string, callerId: number, arg: any) {
   if (typeof arg === "object") {
     console.log("[ui] found object with that is an argument", arg)
     return {
       ...Object.keys(arg).reduce((acc, key) => {
-        acc[key] = checkForCallbacks(fnName, arg[key])
+        acc[key] = checkForCallbacks(fnName, callerId, arg[key])
         return acc
       }, {} as any)
     }
@@ -32,16 +38,28 @@ function checkForCallbacks(fnName: string, arg: any) {
   lastSubscriptionId += 1
   const subscriptionId = lastSubscriptionId
   const unsubscribe = on(
-    `SUB_${fnName}_${subscriptionId}`,
+    getSubscriptionString(fnName, callerId, subscriptionId),
     (subscriptionId: number, ...args: any[]) => {
       // call original callback
       console.log("[ui] received subscription callback", fnName, subscriptionId, args)
       callback(...args)
     }
   )
-  // TODO: need to eventually unsubscribe
+  const prevSubscriptions = subscriptions.get(getSubscriptionStringPrefix(fnName, callerId)) || []
+  subscriptions.set(getSubscriptionStringPrefix(fnName, callerId), [
+    ...prevSubscriptions,
+    unsubscribe
+  ])
 
   return { subscriptionId, fnName: arg.name, __SUBSCRIPTION__: true }
+}
+
+function getSubscriptionString(fnName: string, callerId: number, subscriptionId: number): string {
+  return `${getSubscriptionStringPrefix(fnName, callerId)}_${subscriptionId}`
+}
+
+function getSubscriptionStringPrefix(fnName: string, callerId: number): string {
+  return `SUB_${fnName}_${callerId}`
 }
 
 export function exposeToUI(fn: (...args: any[]) => any) {
@@ -49,19 +67,18 @@ export function exposeToUI(fn: (...args: any[]) => any) {
   console.log("[main] exposing", name)
   on(`REQ_${name}`, async (callerId: number, ...reqArgs: any[]) => {
     console.log("[main] received call", name, callerId, reqArgs)
-    reqArgs = reqArgs.map((arg) => checkForSubscriptions(name, arg))
+    reqArgs = reqArgs.map((arg) => checkForSubscriptions(name, callerId, arg))
     const returnValue = await fn(...reqArgs)
     emit(`RES_${name}_${callerId}`, returnValue)
-    // return () => emit(`REQ_${fnName}_subscriptions_${subscriptionId}`, callerId)
   })
 }
 
-function checkForSubscriptions(fnName: string, arg: any) {
+function checkForSubscriptions(fnName: string, callerId: number, arg: any) {
   if (typeof arg === "object" && !arg.__SUBSCRIPTION__) {
     console.log("[main] found object with that is an argument", arg)
     return {
       ...Object.keys(arg).reduce((acc, key) => {
-        acc[key] = checkForSubscriptions(fnName, arg[key])
+        acc[key] = checkForSubscriptions(fnName, callerId, arg[key])
         return acc
       }, {} as any)
     }
@@ -73,7 +90,7 @@ function checkForSubscriptions(fnName: string, arg: any) {
   const { subscriptionId } = arg
   return (...args: any[]) => {
     console.log("[main] emitting subscription", fnName, subscriptionId, args)
-    emit(`SUB_${fnName}_${subscriptionId}`, subscriptionId, ...args)
+    emit(getSubscriptionString(fnName, callerId, subscriptionId), subscriptionId, ...args)
   }
 }
 
